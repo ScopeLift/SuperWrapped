01b6acc0dbb8b325ef83f20583ac7f7b8dc3838d
Keating <keating.dev@protonmail.com>
2025-03-29 14:49:23 -0400

    Fix ci

--- src/L2NativeSuperchainERC20.sol
+++ src/L2NativeSuperchainERC20.sol
@@ -8,37 +8,37 @@ import {Strings} from "@openzeppelin-contracts/utils/Strings.sol";
 contract L2NativeSuperchainERC20 is SuperchainERC20 {
   using Strings for string;
 
-  string private _name;
-  string private _symbol;
-  uint8 private immutable _decimals;
-  IERC20 public immutable nativeToken;
+  string private NAME;
+  string private SYMBOL;
+  uint8 private immutable DECIMALS;
+  IERC20 public immutable NATIVE_TOKEN;
 
   event Deposit(address indexed from, uint256 amount);
   event Withdrawal(address indexed to, uint256 amount);
 
-  constructor(address nativeToken_) {
-    nativeToken = IERC20(nativeToken_);
-    _name = string.concat("SuperWrapped ", nativeToken.name());
-    _symbol = string.concat("sw", nativeToken.symbol());
-    _decimals = nativeToken.decimals();
+  constructor(address _nativeToken) {
+    NATIVE_TOKEN = IERC20(_nativeToken);
+    NAME = string.concat("SuperWrapped ", NATIVE_TOKEN.name());
+    SYMBOL = string.concat("sw", NATIVE_TOKEN.symbol());
+    DECIMALS = NATIVE_TOKEN.decimals();
   }
 
   function name() public view virtual override returns (string memory) {
-    return _name;
+    return NAME;
   }
 
   function symbol() public view virtual override returns (string memory) {
-    return _symbol;
+    return SYMBOL;
   }
 
   function decimals() public view override returns (uint8) {
-    return _decimals;
+    return DECIMALS;
   }
 
   // Deposit native tokens to receive wrapped tokens
   function deposit(uint256 amount) public payable {
     _mint(msg.sender, amount);
-    nativeToken.transferFrom(msg.sender, address(this), amount);
+    NATIVE_TOKEN.transferFrom(msg.sender, address(this), amount);
     emit Deposit(msg.sender, amount);
   }
 
@@ -46,7 +46,7 @@ contract L2NativeSuperchainERC20 is SuperchainERC20 {
   function withdraw(uint256 amount) public {
     require(balanceOf(msg.sender) >= amount, "Insufficient balance");
     _burn(msg.sender, amount);
-    nativeToken.transfer(msg.sender, amount);
+    NATIVE_TOKEN.transfer(msg.sender, amount);
     emit Withdrawal(msg.sender, amount);
   }
 }
