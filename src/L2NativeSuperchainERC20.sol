36551e908db3b431f454aa9fc896c8e79fe3a20a
wildmolasses <changes@gmail.com>
2025-03-29 16:28:15 -0400

    wip

--- src/L2NativeSuperchainERC20.sol
+++ src/L2NativeSuperchainERC20.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.25;
 import {SuperchainERC20} from "interop-lib/src/SuperchainERC20.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {Strings} from "@openzeppelin-contracts/utils/Strings.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 
 contract L2NativeSuperchainERC20 is SuperchainERC20 {
   using Strings for string;
@@ -12,15 +14,22 @@ contract L2NativeSuperchainERC20 is SuperchainERC20 {
   string private SYMBOL;
   uint8 private immutable DECIMALS;
   IERC20 public immutable NATIVE_TOKEN;
+  PoolKey public key;
+  IPoolManager public poolManager;
 
   event Deposit(address indexed from, uint256 amount);
   event Withdrawal(address indexed to, uint256 amount);
 
-  constructor(address _nativeToken) {
+  constructor(address _nativeToken, address _poolManager) {
     NATIVE_TOKEN = IERC20(_nativeToken);
     NAME = string.concat("SuperWrapped ", NATIVE_TOKEN.name());
     SYMBOL = string.concat("sw", NATIVE_TOKEN.symbol());
     DECIMALS = NATIVE_TOKEN.decimals();
+    poolManager = IPoolManager(_poolManager);
+  }
+
+  function initialize(PoolKey calldata _key) public {
+    key = _key;
   }
 
   function name() public view virtual override returns (string memory) {
@@ -37,6 +46,8 @@ contract L2NativeSuperchainERC20 is SuperchainERC20 {
 
   // Deposit native tokens to receive wrapped tokens
   function deposit(uint256 amount) public payable {
+    poolManager.unlock();
+    poolManager.swap(key, true, -int256(amount), 0x0);
     _mint(msg.sender, amount);
     NATIVE_TOKEN.transferFrom(msg.sender, address(this), amount);
     emit Deposit(msg.sender, amount);
---
+++ test/ConstantSumHook.t.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+import {Fixtures} from "v4-constant-sum/test/utils/Fixtures.sol";
+import {L2NativeSuperchainERC20} from "../src/L2NativeSuperchainERC20.sol";
+import {ConstantSumHook} from "../src/ConstantSumHook.sol";
+import {MockERC20} from "test/mocks/MockERC20.sol";
+
+contract ConstantSumHookTest is Test, Fixtures {
+  ConstantSumHook hook;
+  Currency superchainToken0;
+  Currency nativeToken0;
+  L2NativeSuperchainERC20 superchainToken;
+
+  uint256 tokenId;
+  int24 tickLower;
+  int24 tickUpper;
+
+  function setUp() public {
+    // creates the pool manager, utility routers, and test tokens
+    deployFreshManagerAndRouters();
+    deployMintAndApprove2Currencies();
+
+    deployAndApprovePosm(manager);
+
+    // Create the superchain token
+    nativeToken0 = currency0;
+    superchainToken = new L2NativeSuperchainERC20(Currency.unwrap(nativeToken0));
+    superchainToken0 = Currency.wrap(address(superchainToken));
+
+    // Deploy the hook to an address with the correct flags
+    address flags = address(
+      uint160(
+        Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG
+          | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+      ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+    );
+    bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor
+      // arguments from the hook
+    deployCodeTo("ConstantSumHook.sol:ConstantSumHook", constructorArgs, flags);
+    hook = ConstantSumHook(flags);
+
+    // Create the pool
+    key = PoolKey(nativeToken0, superchainToken0, 3000, 60, IHooks(hook));
+    manager.initialize(key, SQRT_PRICE_1_1);
+
+    // Seed liquidity
+    // IERC20(Currency.unwrap(superchainToken0)).approve(address(hook), 1000e18);
+    // IERC20(Currency.unwrap(nativeToken0)).approve(address(hook), 1000e18);
+    // hook.addLiquidity(key, 1000e18);
+  }
+
+  function test_doesTheSwap(bool zeroForOne, uint256 amount) public {
+    amount = bound(amount, 1 wei, 1000e18);
+
+    MockERC20(Currency.unwrap(nativeToken0)).mint(address(this), amount);
+    IERC20(Currency.unwrap(nativeToken0)).approve(address(hook), amount);
+    superchainToken.deposit(amount);
+
+    assertEq(superchainToken.balanceOf(address(this)), amount);
+    assertEq(nativeToken0.balanceOf(address(hook)), amount);
+    assertEq(superchainToken.balanceOf(address(hook)), 0);
+    assertEq(nativeToken0.balanceOf(address(hook)), 0);
+
+    // swap(key, zeroForOne, -int256(amount), ZERO_BYTES);
+
+    // uint256 balance0After = superchainToken0.balanceOfSelf();
+    // uint256 balance1After = nativeToken0.balanceOfSelf();
+
+    // if (zeroForOne) {
+    //   // paid token0
+    //   assertEq(balance0Before - balance0After, amount);
+
+    //   // received token1
+    //   assertEq(balance1After - balance1Before, amount);
+    // } else {
+    //   // paid token1
+    //   assertEq(balance1Before - balance1After, amount);
+
+    //   // received token0
+    //   assertEq(balance0After - balance0Before, amount);
+    // }
+  }
+
+  //   function test_exactOutput(bool zeroForOne, uint256 amount) public {
+  //     amount = bound(amount, 1 wei, 1000e18);
+  //     uint256 balance0Before = superchainToken0.balanceOfSelf();
+  //     uint256 balance1Before = nativeToken0.balanceOfSelf();
+
+  //     swap(key, zeroForOne, int256(amount), ZERO_BYTES);
+
+  //     uint256 balance0After = superchainToken0.balanceOfSelf();
+  //     uint256 balance1After = nativeToken0.balanceOfSelf();
+
+  //     if (zeroForOne) {
+  //       // paid token0
+  //       assertEq(balance0Before - balance0After, amount);
+
+  //       // received token1
+  //       assertEq(balance1After - balance1Before, amount);
+  //     } else {
+  //       // paid token1
+  //       assertEq(balance1Before - balance1After, amount);
+
+  //       // received token0
+  //       assertEq(balance0After - balance0Before, amount);
+  //     }
+  //   }
+
+  function test_no_v4_liquidity() public {
+    vm.expectRevert();
+    modifyLiquidityRouter.modifyLiquidity(key, LIQUIDITY_PARAMS, ZERO_BYTES);
+  }
+}
