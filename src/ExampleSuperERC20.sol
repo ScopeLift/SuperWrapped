de76ae2dedae3df7544cb1b3b8ff1512e20f29c7
Keating <keating.dev@protonmail.com>
2025-03-29 14:25:59 -0400

    Add tets

--- src/ExampleSuperERC20.sol
+++ src/ExampleSuperERC20.sol
@@ -5,31 +5,31 @@ import {SuperchainERC20} from "interop-lib/src/SuperchainERC20.sol";
 import {Ownable} from "@solady/auth/Ownable.sol";
 
 contract L2NativeSuperchainERC20 is SuperchainERC20, Ownable {
-    string private _name;
-    string private _symbol;
-    uint8 private immutable _decimals;
+  string private _name;
+  string private _symbol;
+  uint8 private immutable _decimals;
 
-    constructor(address owner_, string memory name_, string memory symbol_, uint8 decimals_) {
-        _name = name_;
-        _symbol = symbol_;
-        _decimals = decimals_;
+  constructor(address owner_, string memory name_, string memory symbol_, uint8 decimals_) {
+    _name = name_;
+    _symbol = symbol_;
+    _decimals = decimals_;
 
-        _initializeOwner(owner_);
-    }
+    _initializeOwner(owner_);
+  }
 
-    function name() public view virtual override returns (string memory) {
-        return _name;
-    }
+  function name() public view virtual override returns (string memory) {
+    return _name;
+  }
 
-    function symbol() public view virtual override returns (string memory) {
-        return _symbol;
-    }
+  function symbol() public view virtual override returns (string memory) {
+    return _symbol;
+  }
 
-    function decimals() public view override returns (uint8) {
-        return _decimals;
-    }
+  function decimals() public view override returns (uint8) {
+    return _decimals;
+  }
 
-    function mintTo(address to_, uint256 amount_) external onlyOwner {
-        _mint(to_, amount_);
-    }
+  function mintTo(address to_, uint256 amount_) external onlyOwner {
+    _mint(to_, amount_);
+  }
 }
